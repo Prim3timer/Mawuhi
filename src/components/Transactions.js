28ac50fc76088963de65bd0175d88e4b594420f7
Amalu Ekwelie <67154046+Prim3timer@users.noreply.github.com>
2024-08-13 12:23:38 -0400

    sync removal of item and inventory

--- src/components/Transactions.js
+++ src/components/Transactions.js
@@ -238,7 +238,7 @@ const Transactions = ()=> {
  </div>
  {item.unitMeasure === 'Pounds (lbs)' || item.unitMeasure === 
 
- 'Kilogram (kg)' || 'Litre (L)'? <section><input
+ 'Kilogram (kg)' || item.unitMeasure === 'Litre (L)'? <section><input
  type="text"
  ref={qtyRef}
  placeholder={item.qty}
