30d9bab10bb831f2f51ac9aaa44607113f1e7fdf
Amalu Ekwelie <67154046+Prim3timer@users.noreply.github.com>
2024-08-18 13:18:07 -0400

    finishing touches

--- src/app/api/axios.js
+++ src/app/api/axios.js
@@ -2,5 +2,5 @@ import axios from 'axios'
 
 export default axios.create({
     baseURL: 'http://localhost:3500',
-        // baseURL: 'https://mawuhi-back.onrender.com'
+        baseURL: 'https://mawuhi-back.onrender.com'
 })
