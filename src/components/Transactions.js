8ed3c8f9756e95dcbdcec537fc2a519ff49e7960
Amalu Ekwelie <67154046+Prim3timer@users.noreply.github.com>
2024-08-20 12:05:29 -0400

    more fix

--- src/components/Transactions.js
+++ src/components/Transactions.js
@@ -260,9 +260,7 @@ const Transactions = ()=> {
                     >Qty:</h4>
                 
  </div>
- {item.unitMeasure === 'Pounds (lbs)' || item.unitMeasure === 
-
- 'Kilogram (kg)' || item.unitMeasure === 'Litres (L)' ? <section>
+ <section>
     <input
  type="text"
  ref={qtyRef}
@@ -279,31 +277,7 @@ onClick={() => dispatch({type: 'qty', payload: ''})}
  }}
  >{item.unitMeasure.split(' ')[1]}</span>
   
- </section> :
- <section>
-      <svg 
-     onClick={()=> upper(item._id)}
-      xmlns='http://www.w3.org/2000/svg'
-       viewBox='0 0 20 20'
-    width='80px'
- height='70px'
-
-      >
-<path d='M10.707 7.05L10 6.343 4.343 12l1.414 1.414L10 9.172l4.243 4.242L15.657 12z' />
-</svg>
- <h3 id="qty-header">{item.qty} {item.unitMeasure.split(' ')[1]} </h3>
-
-      <svg 
-     onClick={()=> downer(item._id)}
-  width='80px'
-  height='70px'
-
-      xmlns='http://www.w3.org/2000/svg'
-       viewBox='0 0 20 20'
-      >
-<path d='M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z' />
-</svg>
- </section>}
+ </section> 
  
                    </article>
                     <article>
