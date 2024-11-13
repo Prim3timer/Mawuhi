24048967d66e11ef8cdff925027a38621fd1aab7
Amalu Ekwelie <67154046+Prim3timer@users.noreply.github.com>
2024-11-12 23:22:59 -0500

    item table margin

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -20,7 +20,7 @@ const ItemList = ()=> {
     const [state, dispatch] = useReducer(reducer, initialState)
     // const [mark, setMark] = useState('')  
 
-    const measurements = ['Kilograms (kg)', 'Pieces (pcs)', 'Plates (Plts)', 'Dozen (dzn)', 'Bottles (Btl)', 'Pounds (lbs)', 'Litres (L)', 'Sachet (sct)', 'Ounces (Oz)', 'grams (g)'
+    const measurements = ['Kilogram (kg)', 'Piece (pc)', 'Plate (Plt)', 'Dozen (dzn)', 'Bottle (Btl)', 'Pound (lbs)', 'Litre (L)', 'Sachet (sct)', 'Ounce (Oz)', 'Gram (g)'
        ]
     const itemRef = useRef()
     const getTrans = async ()=> {
