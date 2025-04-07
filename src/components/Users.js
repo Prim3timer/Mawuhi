72a28c4bbbc715c960978d5c378b5f64c1ec613d
Amalu Ekwelie <67154046+Prim3timer@users.noreply.github.com>
2025-04-06 23:43:42 -0400

    done but one

--- src/components/Users.js
+++ src/components/Users.js
@@ -9,6 +9,7 @@ import reducer from "../reducer";
 import { useReducer } from "react";
 const Users = ()=> {
     const [users, setUsers] = useState()
+    const [madu, setMadu] = useState()
     const [state, dispatch] = useReducer(reducer, initialState)
     const [currenPerson, setCurrentPerson] = useState()
     const [selected, setSelected] = useState(false)
@@ -18,7 +19,7 @@ const Users = ()=> {
             console.log(users)
             auth.picker2 = id
             console.log(auth.picker2)
-            const currentUser = users.find((user) => user._id = auth.picker)
+            const currentUser = users.find((madu) => madu._id = auth.picker)
             dispatch({type: 'backendUser', payload: currentUser})
             console.log(state.backendUser)
         }
@@ -26,18 +27,73 @@ const Users = ()=> {
     const removeUser = async (id) => {
 
         await axios.delete(`/users/delete/${id}`)
-        const person = users && users.filter((user) => user._id !== id)
+        const person = users && users.filter((madu) => madu._id !== id)
         setUsers(person)
        
         // setCurrentPerson(person)
         console.log(currenPerson)
     }
+
+
+const assertain = (id) => {
+    if (auth.roles.includes(5150)){
+        console.log("deleted")
+        
+        dispatch({type: 'cancel', payload: true})
+        console.log(state.cancel)
+        // dispatch({type: 'id', payload: id})
+        const getItem = users.find((person)=> person._id === id)
+        setMadu(getItem)
+        console.log(madu)
+    }
+    else {
+        // dispatch({type: 'isMatched', payload: true})
+    }
+}
+
+
+const handleRemove = async ()=> {
+    
+    
+    const response = await axios.delete(`/users/delete/${madu._id}`)
+    if (response){
+        
+        dispatch({type: 'cancel', payload: false})
+        const newGraw =  users.filter((item)=> item._id !== madu._id)
+        setUsers(newGraw)
+}
+}
+
+
+const remainDelete = ()=> {
+    // this condition statement is to enable the removal of the confirm window once any part of the 
+    // page is touched.
+    if (state.cancel){
+
+        dispatch({type: 'cancel', payload: false})
+    }
+
+
+    // if (state.isEdit){
+        
+    //     dispatch({type: 'isEdit', payload: false})
+    // }
+}
+const generalRemain = () => {
+    if (state.isMatched) dispatch({type: 'isMatched', payload: false})
+
+ } 
+const grabId = (id) => {
+    console.log(id)
+}
+
 useEffect(()=> {
    
     let isMounted = true
     const controller = new AbortController()
 
     const getUsers = async ()=> {
+      
         try {
             const response = await axios.get('/users', {
                 signal: controller.signal
@@ -54,6 +110,7 @@ useEffect(()=> {
         }
     }
 console.log(users)
+
     getUsers()
     // clean up function
     return ()=> {
@@ -89,21 +146,21 @@ return (
                 <th>Delete</th>
             </tr>
           
-                {users.map((user, index)=> {
+                {users.map((madu, index)=> {
                     return <tr key={index}
-                    // onClick={() => userPage(user._id)}
+                    // onClick={() => userPage(madu._id)}
                     style={{backgroundColor: index % 2 === 0 ?
                         'white' : 'lightsalmon'}}       
                     >
 
                         <th 
-                        onClick={() => userPage(user._id)}
-                        ><Link to="/user-select">{user?.username}</Link></th>
+                        onClick={() => userPage(madu._id)}
+                        ><Link to="/user-select">{madu?.username}</Link></th>
                         <td
-                         onClick={() => userPage(user._id)}
-                        >{user?._id}</td>
+                         onClick={() => userPage(madu._id)}
+                        >{madu?._id}</td>
                          <td
-                                                onClick={()=> removeUser(user._id)}
+                                                onClick={()=> assertain(madu._id)}
                                             >
                                             <FaTrashAlt role='button'
                                             tableIndex='0'/> 
@@ -116,7 +173,73 @@ return (
                )}
                 </tbody>
            </table>
-        ) : <p>no user to display</p>}
+        ) : <p>no madu to display</p>}
+
+
+<div
+            style={{
+                display: `${state.cancel ? 'block' : 'none'}`,
+                position: 'absolute',
+            textAlign: 'center',
+            top: '35%',
+            left: '5%',
+            width: '90%',
+             padding: '1rem',
+               backgroundColor: '#DBBFDB',
+               borderRadius: '5px',
+               opacity: '.85'
+         }}
+         >
+             <h3
+          id="verify-header"
+          style={{
+              margin: '.5rem auto',
+            //   display: 'flex',
+          }}
+          >Are you sure you want to delete: {madu && madu.username}</h3>
+                 <article
+                 style={{
+                     display: 'flex',
+                    //  flexDirection: 'row',
+                     columnGap: '4vw',
+                     justifyContent: 'center',
+                 }}
+                 >
+                    <button
+                 onClick={remainDelete}
+                 >No</button><button
+                  onClick={handleRemove}
+                 style={{backgroundColor: 'red',
+                     borderColor: 'red'
+                 }}
+                 >Yes</button></article></div> 
+
+<div
+        style={{
+            display: `${state.isMatched ? 'block' : 'none'}`,
+            position: 'absolute',
+        textAlign: 'center',
+        top: '35%',
+        left: '5%',
+        width: '90%',
+         padding: '1rem',
+           backgroundColor: '#DBBFDB',
+           borderRadius: '5px',
+           opacity: '.85'
+     }}
+     >
+         <h2
+      id="verify-header"
+      style={{
+          margin: '.5rem auto',
+        //   display: 'flex',
+      }}
+      >Unauthorized!</h2>
+      <button 
+      onClick={generalRemain}
+       >
+        ok</button>
+            </div>
     </article>
 )
 }
