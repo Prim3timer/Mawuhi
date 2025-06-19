53c1270e0edb56a659bce3ab7c93280a823d9832
Amalu Ekwelie <67154046+Prim3timer@users.noreply.github.com>
2025-06-18 21:00:56 -0400

    stripping

--- src/Thanks.js
+++ src/Thanks.js
@@ -8,7 +8,7 @@ const Thanks = () =>{
 const {auth} = useAuth()
 console.log(auth.cartArray)
 const getRecipt = async ()=> {
-    const response = await axios.get('/thanks')
+    const response = await axios.get('/transactions')
     // setAlert(response.data)
     console.log(response.data)
     console.log('hoolaa')
