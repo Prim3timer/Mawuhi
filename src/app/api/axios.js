eb68d1242ea3785e1dd7d8c9d7c2e0671a54c461
Amalu Ekwelie <67154046+Prim3timer@users.noreply.github.com>
2025-06-19 10:31:31 -0400

    stripping

--- src/app/api/axios.js
+++ src/app/api/axios.js
@@ -1,10 +1,10 @@
 import axios from 'axios'
-// import dotenv from 'dotenv'
+// import dotenv from 'dotenv'  
 
 // dotenv.config()
 
 export default axios.create({
-    baseURL: 'http://localhost:3500',
-    // baseURL: 'https://mawuhi-back.onrender.com'
+    // baseURL: 'http://localhost:3500',
+    baseURL: 'https://mawuhi-back.onrender.com'
 })
 
