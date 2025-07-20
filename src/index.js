d8a278904ffa8b311c6f3d6791c65d557844534c
Amalu Ekwelie <67154046+Prim3timer@users.noreply.github.com>
2025-07-20 17:38:24 -0400

    phew

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ import { AuthProvider } from './context/authProvider';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     // <Provider store={store}>
-  <React.StrictMode>
+  // <React.StrictMode>
     <BrowserRouter>
     <AuthProvider>
       <Routes>
@@ -25,7 +25,7 @@ root.render(
       </Routes>
     </AuthProvider>
     </BrowserRouter>
-  </React.StrictMode>
+  // </React.StrictMode>
   // </Provider>
 );
 
