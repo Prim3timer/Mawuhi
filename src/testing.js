2830f438872065f6d10b94405a0e8a1e15c45af0
Amalu Ekwelie <67154046+Prim3timer@users.noreply.github.com>
2025-07-26 20:38:13 -0400

    stubborn

--- src/testing.js
+++
@@ -1,7 +0,0 @@
-
-
-const sentence = ` '12490.99',
-    itemDets: '[{"transQty":1,"id":"684d02c69d1d73623e83c526"},{"transQty":1,"id":"684c6eec9d1d73623e83c345"},{"transQty":1,"id":"6866f890df2fa18c47fce352"},{"transQty":1,"id":"68589fafd815abfa7fa95145"}]'`
-
-
-    console.log(sentence.length)
