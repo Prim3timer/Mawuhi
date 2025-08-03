90245865227540d1e948415c2b7bc1666c81c462
Amalu Ekwelie <67154046+Prim3timer@users.noreply.github.com>
2025-08-03 11:59:57 -0400

    user select and state retaintion

--- src/components/Login.js
+++ src/components/Login.js
@@ -58,7 +58,6 @@ const Login = () => {
             const accessToken = response?.data?.accessToken;
             const roles = response?.data?.roles;
             const picker = response?.data?.id;
-            const user = response?.data?.username
             
             setAuth({ user,roles, accessToken, picker});
             
