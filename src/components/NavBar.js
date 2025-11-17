20ba015d2f7c4346ea0f9427ea800454d070dfdf
Amalu Ekwelie <67154046+Prim3timer@users.noreply.github.com>
2025-11-16 20:31:33 -0500

    trillion

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -71,13 +71,9 @@ const logout = useLogout()
             {auth.accessToken && mainLinks.map((mainLink)=> {
                     const {id, name, path} = mainLink
                 return (
-                  <div className="home-names">
 
                     <Link to={path} className="home-links" key={id}>{name}</Link>
-                    {/* <br/> */}
-                    {/* <br/> */}
-                    {/* <p>|</p> */}
-                  </div>
+               
                       
                     )
                 })}
